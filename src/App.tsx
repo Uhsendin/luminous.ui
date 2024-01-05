7ec99150fe86e75d86b95a72b0a2107d053e5008
Brandon Rodriguez <chilebr99@gmail.com>
2024-01-04 20:12:10 -0500

    Created navbar skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { SiGithub } from 'react-icons/si'
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<header className="w-full border-b bg-white">
+			<div className="flex h-16 items-center px-10 sm:px-16 lg:px-44">
+				<div className="mx-auto w-full max-w-3xl space-y-20">
+					<div className="flex justify-between">
+						<div className="flex flex-1 items-center justify-start">
+							<a
+								href="/"
+								className="inline-flex h-10 items-center justify-center text-lg font-bold text-gray-800"
+							>
+								LuminousUi
+							</a>
+						</div>
+						<div className="flex flex-1 items-center justify-end">
+							<nav className="flex items-center space-x-1">
+								<a
+									href="https://github.com/Uhsendin/luminous.ui"
+									className="h-10 w-10 p-2 text-gray-800 hover:text-[#4078c0]"
+								>
+									<SiGithub className="h-full w-full" />
+								</a>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
 }
 
 export default App
