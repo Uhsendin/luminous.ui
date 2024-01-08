80bcbae1da3baef371a1e9eacf446b47398f638e
Brandon Rodriguez <chilebr99@gmail.com>
2024-01-08 15:01:23 -0500

    Added comments & more sections

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,10 @@ function App() {
 					</div>
 				</div>
 			</header>
-
+			{/* Main content */}
 			<main className="p-10 sm:px-16 lg:px-44">
 				<div className="mx-auto max-w-3xl space-y-20">
+					{/* Content container */}
 					<header>
 						<h1 className="black text-2xl font-bold text-gray-800 sm:text-3xl">
 							Component name
@@ -38,12 +39,15 @@ function App() {
 						<p className="mt-2 text-lg text-gray-800">Component description</p>
 					</header>
 					<div className="space-y-10 md:space-y-16">
+						{/* Content section */}
 						<div>
+							{/* Section heading */}
 							<h2 className="text-lg font-bold text-gray-800">
 								Section heading
 							</h2>
-
+							{/* Section description */}
 							<p className="mt-1 text-gray-600">Section description text</p>
+							{/* Section preview */}
 							<div className="mt-3">
 								<div className="flex flex-col rounded-xl border p-6 shadow-sm">
 									<div className="flex flex-wrap gap-2">
@@ -53,7 +57,73 @@ function App() {
 								</div>
 							</div>
 						</div>
-						<div></div>
+
+						{/* Content section */}
+						<div>
+							{/* Section heading */}
+							<h2 className="text-lg font-bold text-gray-800">
+								Section heading
+							</h2>
+							{/* Section description */}
+							<p className="mt-1 text-gray-600">Section description text</p>
+							{/* Section preview */}
+							<div className="mt-3">
+								<div className="flex flex-col rounded-xl border p-6 shadow-sm">
+									<div className="flex flex-wrap gap-2">
+										<div className="bg-blue-500/50 p-3">Component 1</div>
+										<div className="bg-blue-500/50 p-3">Component 1</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						{/* Content section */}
+					</div>
+					{/* Content container */}
+					<header>
+						<h1 className="black text-2xl font-bold text-gray-800 sm:text-3xl">
+							Component name
+						</h1>
+						<p className="mt-2 text-lg text-gray-800">Component description</p>
+					</header>
+					<div className="space-y-10 md:space-y-16">
+						{/* Content section */}
+						<div>
+							{/* Section heading */}
+							<h2 className="text-lg font-bold text-gray-800">
+								Section heading
+							</h2>
+							{/* Section description */}
+							<p className="mt-1 text-gray-600">Section description text</p>
+							{/* Section preview */}
+							<div className="mt-3">
+								<div className="flex flex-col rounded-xl border p-6 shadow-sm">
+									<div className="flex flex-wrap gap-2">
+										<div className="bg-blue-500/50 p-3">Component 1</div>
+										<div className="bg-blue-500/50 p-3">Component 1</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						{/* Content section */}
+						<div>
+							{/* Section heading */}
+							<h2 className="text-lg font-bold text-gray-800">
+								Section heading
+							</h2>
+							{/* Section description */}
+							<p className="mt-1 text-gray-600">Section description text</p>
+							{/* Section preview */}
+							<div className="mt-3">
+								<div className="flex flex-col rounded-xl border p-6 shadow-sm">
+									<div className="flex flex-wrap gap-2">
+										<div className="bg-blue-500/50 p-3">Component 1</div>
+										<div className="bg-blue-500/50 p-3">Component 1</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						{/* Content section */}
 					</div>
 				</div>
 			</main>
